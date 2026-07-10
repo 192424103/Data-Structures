d7fde0c202e018c2234c909744729cc9a589928d
Nithya Sree <nithyasreegolamary@gmail.com>
2026-07-10 13:50:43 +0530

    Add Fibonacci series program in EXP-4.c

---
+++ EXP-4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,a=0,b=1,c,i;
+
+    printf("Enter number of terms: ");
+    scanf("%d",&n);
+
+    printf("Fibonacci Series:\n");
+
+    for(i=1;i<=n;i++)
+    {
+        if(i==1)
+            printf("%d ",a);
+        else if(i==2)
+            printf("%d ",b);
+        else
+        {
+            c=a+b;
+            printf("%d ",c);
+            a=b;
+            b=c;
+        }
+    }
+
+    return 0;
+}
